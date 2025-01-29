dac40e882aaf1f7215db82aa80335811c9ff5a9f
suchmolin <suchmolin11@gmail.com>
2025-01-29 17:56:42 -0400

    actualizacion de sectores de empleo

--- src/data/sectoresempleo.js
+++ src/data/sectoresempleo.js
@@ -1,82 +1,55 @@
 export const sectoresempleo = [
   {
-    id: "docencia",
-    titulo: "Docencia",
+    id: "docenciaeinvestigacion",
+    titulo: "Docencia e Investigación",
     img: "/img/sectoresdeempleo/docencia.png",
   },
   {
-    id: "ingenieria",
-    titulo: "Ingeniería",
+    id: "ingenieriayconstruccion",
+    titulo: "Ingeniería y Construcción",
     img: "/img/sectoresdeempleo/ingenieria.png",
   },
   {
-    id: "administracion",
-    titulo: "Administración / Oficina",
+    id: "administracionycontabilidad",
+    titulo: "Administración y Contabilidad",
     img: "/img/sectoresdeempleo/administrativo.jpg",
   },
   {
-    id: "informatica",
-    titulo: "Informática / Telecomunicaciones",
+    id: "tecnologiaytelecomunicaciones",
+    titulo: "Tecnología y Telecomunicaciones",
     img: "/img/sectoresdeempleo/informatica.png",
   },
   {
-    id: "atencionalcliente",
-    titulo: "Atención a clientes",
+    id: "atencionalclienteyventas",
+    titulo: "Atención a clientes y Ventas",
     img: "/img/sectoresdeempleo/atencionalcliente.jpg",
   },
   {
-    id: "serviciosgenerales",
-    titulo: "Servicios Generales, Aseo y Seguridad",
+    id: "serviciosgeneralesymantenimiento",
+    titulo: "Servicios Generales y Mantenimiento",
     img: "/img/sectoresdeempleo/serviciosgenerales.jpg",
   },
   {
-    id: "investigacionycalidad",
-    titulo: "Investigación y Calidad",
-    img: "/img/sectoresdeempleo/investigacionycalidad.jpg",
-  },
-  {
     id: "direccionygerencia",
-    titulo: "Dirección / Gerencia",
+    titulo: "Dirección y Gerencia",
     img: "/img/sectoresdeempleo/direccionygerencia.jpg",
   },
+
   {
-    id: "mantenimeintoyrepacionestecnicas",
-    titulo: "Mantenimiento y Reparaciones Técnicas",
-    img: "/img/sectoresdeempleo/mantenimientoyreparacionestecnicas.jpg",
-  },
-  {
-    id: "produccionoperariosmanufactura",
-    titulo: "Producción / Operarios / Manufactura",
+    id: "produccionymanufactura",
+    titulo: "Producción y Manufactura",
     img: "/img/sectoresdeempleo/produccionoperariosmanufactura.jpg",
   },
+
   {
-    id: "contabilidadfinanzas",
-    titulo: "Contabilidad / Finanzas",
-    img: "/img/sectoresdeempleo/contabilidadfinanzas.jpg",
-  },
-  {
-    id: "hosteleriaturismo",
-    titulo: "Hostelería / Turismo",
+    id: "hosteleriaturismorestaurantes",
+    titulo: "Hostelería, Turismo y Restaurantes",
     img: "/img/sectoresdeempleo/hosteleriaturismo.jpg",
   },
+
   {
-    id: "ventas",
-    titulo: "Ventas",
-    img: "/img/sectoresdeempleo/ventas.png",
-  },
-  {
-    id: "callcentertelemercadeo",
-    titulo: "CallCenter / Telemercadeo",
+    id: "marketingycomunicaciones",
+    titulo: "Marketing y Comunicaciones",
     img: "/img/sectoresdeempleo/callcentertelemercadeo.jpg",
-  },
-  {
-    id: "construccionobras",
-    titulo: "Construcción y obra",
-    img: "/img/sectoresdeempleo/construccionobras.jpg",
-  },
-  {
-    id: "restaurantes",
-    titulo: "Restaurantes",
-    img: "/img/sectoresdeempleo/restaurantes.jpg",
   },
 ]
