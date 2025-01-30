9c499b912d6cb780b4b2c6d71a77beb9d72a1b3e
suchmolin <suchmolin11@gmail.com>
2025-01-30 10:51:26 -0400

    [test] select professions5

--- src/functions/get/getSelectProfession.js
+++ src/functions/get/getSelectProfession.js
@@ -1,7 +1,7 @@
 export default async function getSelectProfession() {
   const resp = await fetch("/api/selectProfession")
   const json = await resp.json()
-  console.log({ profession: resp.body.json() })
+  console.log({ profession: JSON.parse(resp.body) })
 
   return json
 }
--- src/functions/get/getSelectTypeId.js
+++ src/functions/get/getSelectTypeId.js
@@ -1,6 +1,6 @@
 export default async function getSelectTypeId() {
   const resp = await fetch("/api/selectTypeId")
-  console.log({ typeid: resp.body.json() })
+  console.log({ typeid: JSON.parse(resp.body) })
   const json = await resp.json()
 
   return json
